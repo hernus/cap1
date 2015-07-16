ff6168ff63902c40f175df9318dd663b46818341
Hernus Carelsen <hernus@carelsen.com>
2015-07-16 15:23:50 +1000

    adsf

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,10 +1,10 @@
 set :user, "vagrant"
 
 server '192.168.33.10',
-user: 'vagrant',
+user: fetch(:user , "deploy"),
 roles: %w{web app db},
 ssh_options: {
-  user: 'vagrant', 
+  user: fetch(:user, "deploy"), 
   # keys: ['~/.vagrant.d/insecure_private_key'],
   # forward_agent: true,
   # auth_methods: %w(publickey password)

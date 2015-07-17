7c4efc8cddfa86b1456589e101ad1c026ed196c1
Hernus Carelsen <hernus@carelsen.com>
2015-07-17 14:33:55 +1000

    adf

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # config valid only for current version of Capistrano
 lock '3.4.0'
 
-set :application, 'my_app_name'
+set :application, 'cap1'
 set :repo_url, 'https://github.com/hernus/cap1.git'
 
 # Default branch is :master
@@ -56,85 +56,23 @@ namespace :deploy do
     end
   end
 
+  task :site_symlink do
+    on roles(:web) do
+      if remote_path_exists?("/etc/nginx/sites-enabled/default") do
+        execute :sudo, :rm, "/etc/nginx/sites-enabled/default"
+      end
+      unless remote_path_exists?("/etc/nginx/sites-enabled/romeo") do
+        execute :sudo, :ln, "-s /etc/nginx/sites-available/romeo /etc/nginx/sites-enabled/romeo"
+      end
+    end
+  end
+
   after :publishing, 'deploy:assets_precompile'
+  after :publishing, 'deploy:site_symlink'
 
 end
 
-
-# --------------------------------------------------------------------
-
-namespace :provision do
-
-   ruby_v = "ruby-2.0.0-p247"
-   rvm_env = {
-     'PATH' => "/usr/local/rvm/gems/#{ruby_v}/bin:/usr/local/rvm/rubies/#{ruby_v}/bin:/usr/local/rvm/bin:$PATH",
-     'RUBY_VERSION' => ruby_v, 
-     'GEM_HOME'     => "/usr/local/rvm/gems/#{ruby_v}",
-     'GEM_PATH'     => "/usr/local/rvm/gems/#{ruby_v}",
-     'BUNDLE_PATH'  => "/usr/local/rvm/gems/#{ruby_v}"
-   }
-
-   # set :pty, true
-
-   task :create_site_directory do
-      on roles(:web) do |host|
-         execute "cd / && sudo mkdir sites && sudo chown #{deploy_user} sites"
-      end
-   end
-
-   task :install_rvm do
-      on roles(:web) do |host|
-          execute  "sudo apt-get update"
-          execute  "gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3"
-          execute  '\curl -sSL https://get.rvm.io | sudo bash -s stable'
-          execute  'sudo adduser vagrant rvm' 
-          execute  "sudo", "/usr/local/rvm/bin/rvm install #{ruby_v}"
-          execute  "sudo /usr/local/rvm/bin/rvm alias create default #{ruby_v}"
-      end
-   end
-
-
-   set :default_env, rvm_env
-   task :install_chef do
-      on roles(:web) do |host|
-        # execute  'sudo', '/usr/local/rvm/bin/rvm all do gem install bundler --no-rdoc --no-ri'
-        # execute  'sudo', '/usr/local/rvm/bin/rvm all do gem install chef --no-rdoc --no-ri'
-
-        # execute  'sudo', 'apt-get install git -y'
-        execute  'cd /sites && mkdir chef'
-      end
-   end
-
-
-   set :default_env, rvm_env
-   task :chef_execute do
-      on roles(:web) do |host|
-          within "/sites/chef" do
-             execute  :git, :pull
-             execute  :sudo , "chef-solo",  '-c solo.rb -j node.json'       
-          end
-      end
-   end
-
-
-   # set :default_env, rvm_env
-   # task :prepare_app do
-   #    on roles(:web) do |host|
-   #       within "/vagrant" do
-   #          execute  :bundle, "install"
-   #          execute  :rake,   "db:create"
-   #          execute  :rake,   "db:schema:load"
-   #       end
-   #    end
-   # end
-
+def remote_path_exists?(path)
+  capture("if [ -e '#{path}' ]; then echo -n 'true'; fi").include?("true")
 end
-
-task :provision do
-   # invoke 'provision:create_site_directory'
-   # invoke 'provision:install_rvm'
-   # invoke 'provision:install_chef'
-   invoke 'provision:chef_execute'
-end
-
 
